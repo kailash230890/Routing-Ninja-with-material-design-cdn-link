f4e8322807f2230e4b97f639944813f58f07c0d7
Kailash <53666209+kailash230890@users.noreply.github.com>
2020-01-28 20:21:09 +0530

    Add files via upload

---
+++ Routing-Ninja/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        officiis blanditiis temporibus magnam tempora id veritatis autem libero
+        itaque, numquam nulla iste maxime laudantium provident corporis, optio
+        molestias ipsam ratione omnis ab? Recusandae quae animi adipisci vitae!
+        Ipsum consequuntur harum assumenda quos eius enim sequi provident est,
+        quas velit corporis.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
